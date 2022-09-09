1235767ca2cd74dcf99a0e81c6ee1ce3f9917b03
user <andrei-sinyac@mail.ru>
2022-09-09 12:17:02 +0300

    File_2

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -7,6 +7,12 @@ def index(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
     city = 'Minsk'
-    res = requests.get(url.format(city))
-    print(res.text)
-    return render(request, 'weather/index.html')
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
